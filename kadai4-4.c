686e46297d030eb59813a118c135a92fcf247a2a
t180062 <49541460+t180062@users.noreply.github.com>
2019-05-10 12:17:24 +0900

    Update kadai4-4.c

--- kadai4-4.c
+++ kadai4-4.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(){
+    double N,i,sum=1;
+    double a=1;
+    printf("N:");
+    scanf("%lf",&N);
+    for(i=0;i<=N;i++){
+        printf("%lf\n",sum);
+        a=a/2;
+        sum=sum+a;
+    }
+}
