aff03afc426b78b67e96cba06114dfac56eeb1c8
t180062 <49541460+t180062@users.noreply.github.com>
2019-05-10 12:16:36 +0900

    Update kadai4-2.c

--- kadai4-2.c
+++ kadai4-2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(){
+    int N,i;
+    int a=1;
+    printf("N:");
+    scanf("%d",&N);
+    printf("%d\n",a);
+    for(i=0;i<N;i++){
+        a=a*2;
+    printf("%d\n",a);
+    }
+}
