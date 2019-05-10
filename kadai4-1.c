f2cf3f9bfcb40947180843e2c1b1106d4e6027c4
t180062 <49541460+t180062@users.noreply.github.com>
2019-05-10 12:15:57 +0900

    Update kadai4-1.c

--- kadai4-1.c
+++ kadai4-1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(){
+    int N,i;
+    int sum=0;
+    printf("N:");
+    scanf("%d",&N);
+    printf("%d\n",sum);
+    for(i=0;i<N;i++){
+        sum=sum+2;
+    printf("%d\n",sum);
+    }
+}
